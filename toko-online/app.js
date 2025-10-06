6f4d5d52d39e5c365a8481fe3b544997e91905dd
Josan <Jonathanss999@gmail.com>
2025-10-06 17:00:13 +0700

    update mongodb

--- toko-online/app.js
+++ toko-online/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 //Perbaikan ke 2
 var indexRouter = require('./app_toko_online/routes/index');
 var usersRouter = require('./app_toko_online/routes/users');
-var productRouter = require('./app_toko_online/routes/product'); 
+var productRouter = require("./app_toko_online/routes/product"); //letakkan di atas agar rapi
 var engine = require('ejs-blocks'); //menggunakan ejs block
 var app = express();
 
@@ -26,7 +26,6 @@ app.use('/bootstrap', express.static(path.join(__dirname,'node_modules/bootstrap
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-var productRouter = require('./routes/product'); 
 app.use("/produk", productRouter);
 
 // catch 404 and forward to error handler
